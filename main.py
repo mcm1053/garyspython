9ea8817dc9416ee04aebd225add2b4e89facaa62
mcm1053 <mcm1053@proton.me>
2022-11-10 17:21:01 -0600

    while loop lmao

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from datetime import date
 
 # Declarations
 today = date.today()
+tofile = []
 
-
+# Conversion for simple math
 def convert(args):
     try:
         result = tuple(map(float, args))
@@ -12,14 +13,14 @@ def convert(args):
     except ValueError:
         return False, 'Wrong number input !'
 
-
+# Input frame
 def input_frame():
     return [
         [sg.Text(item, size=size1, pad=(0, 0)),
          sg.Input('0.00', size=size2, enable_events=True, key=item, expand_x=True)]
         for item in input_items]
 
-
+# Output frame
 def output_frame():
     return [
         [sg.Text(item, size=size1, pad=(0, 0)),
@@ -41,16 +42,17 @@ date = ("Date",)
 # r[4]  = Wholesale / Gift Card
 # r[5]  = Register Tax
 # r[6]  = Layaways Including Tax
-# r[7]  = Purchase
-# r[8]  = New Pawns
-# r[9]  = Bank Deposits
-# r[10] = Cash Pd Out Supplies
-# r[11] = Freight + Postage
-# r[12] = Yard + Pest Control
-# r[13] = Gift Card Redeemed
-# r[14] = Misc
+# r[7]  = Total COH
+# r[8]  = Purchase
+# r[9]  = New Pawns
+# r[10]  = Bank Deposits
+# r[11] = Cash Pd Out Supplies
+# r[12] = Freight + Postage
+# r[13] = Yard + Pest Control
+# r[14] = Gift Card Redeemed
+# r[15] = Misc
 input_items = ('Sales', 'Layaways', 'Pawn Fees', 'Pawn Redeem', 'Wholesale / Gift Card',
-               'Register Tax', 'Layaways Including Tax', 'Purchase', 'New Pawns', 'Bank Deposits', 'Cash PD Out Supplies', 'Freight & Postage', 'Yard & Pest Control', 'Gift Card Redeemed', 'Misc')
+               'Register Tax', 'Layaways Including Tax', 'Total COH', 'Purchase', 'New Pawns', 'Bank Deposits', 'Cash PD Out Supplies', 'Freight & Postage', 'Yard & Pest Control', 'Gift Card Redeemed', 'Misc')
 
 # Right column
 output_items = ('Beginning COH', 'Total Register Sales', 'Layaway Tax',
@@ -62,16 +64,16 @@ sg.SetOptions(font=('Arial', 10, 'bold'))
 
 # Window layout
 layout = [
-    [sg.Input(key='-INPUT-'),
-     sg.FileBrowse(file_types=(("TXT Files", "*.txt"), ("ALL Files", "*.*"))),
-     sg.Button("Open"),
-     ],
-    # [sg.Button('Date', button_color='red', key='Open')],
-    [sg.Text()],
     [sg.Frame('Input',  input_frame(),  vertical_alignment='top', expand_y=True, expand_x=True),
      sg.Frame('Output', output_frame(), vertical_alignment='top', expand_y=True, expand_x=True)],
     [sg.Text()],
-    [sg.Button('Save & Exit', key='Exit')]
+    [
+        sg.Button('Save & Exit', key='Exit'),
+        sg.Input(key='-INPUT-'),
+        sg.FileBrowse(file_types=(
+            ("TXT Files", "*.txt"), ("ALL Files", "*.*"))),
+        sg.Button('Open', key='Open'),
+    ],
 ]
 
 # Window setup
@@ -84,11 +86,21 @@ for item in output_items + ('Exit',):
 while True:
     event, values = window.read()
     # Hitting exit or close saves to file + closes
-    # TODO: export to excel sheets based on date
+    # TODO: Export to excel sheets based on date
     if event in ('Exit',  sg.WIN_CLOSED):
-        f = open("TMP.txt", "w")
-        f.write(f"{ecoh}\n")
-        f.close()
+        # Add all variables to a list
+        i = 0
+        while i < 15:
+            tofile.append(r[i])
+            i += 1
+        # tofile.append(r[0])
+        # tofile.append(r[1])
+        # Write all vars to a file:
+        # TODO: Make the name the current date
+        with open("TMP.txt", "w") as f:
+            for line in tofile:
+                f.write(f"{line}\n")
+            f.close()
         break
     # Enter / tab goes to next cell
     if event == 'Return':
@@ -96,6 +108,7 @@ while True:
         user_event.widget.tk_focusNext().focus()
         user_event.widget.tk_focusNext().select = True
         select = True
+    # TODO: Setup open functionality
     if event == 'Open':
         filename = values['-INPUT-']
         if Path(filename).is_file():
@@ -111,8 +124,9 @@ while True:
         if not status:
             window['Status'].update(r)
             continue
-        # Total Register Sales = Sales + Layaways
+        # TODO: If file is opened from another date, fill variables. else, use ecoh from yesterday as todays bcoh
         bcoh = 30909.39
+        # Total Register Sales = Sales + Layaways
         trs = round(r[0]+r[1], 2)
         # Layaway Tax = Layaways Including Tax - Layaways
         lt = round(r[6]-r[1], 2)
@@ -120,8 +134,10 @@ while True:
         ttc = round(r[5] + lt, 2)
         # Ending Cash on Hand = bcoh+sum(trs+ttc+r[2]+r[3]+r[4])-sum(r[7]-r[14])
         leftECOH = [trs, ttc, r[2], r[3], r[4]]
-        rightECOH = [r[7], r[8], r[9], r[10], r[11], r[12], r[13], r[14]]
+        rightECOH = [r[8], r[9], r[10], r[11], r[12], r[13], r[14], r[15]]
         ecoh = round(bcoh+sum(leftECOH)-sum(rightECOH), 2)
+        # Over or Short = Total Cash on Hand - Ending Cash on Hand
+        oos = round(r[7]-ecoh, 2)
         for item, value in zip(output_items, (bcoh, trs, lt, ttc, ecoh)):
             window[item].update(value)
 
