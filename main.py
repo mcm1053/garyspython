8eb9593fa2387e1846a337ad373f1c3b2e797f08
mcm1053 <89549880+mcm1053@users.noreply.github.com>
2022-11-29 13:56:27 -0600

    Update main.py

--- main.py
+++ main.py
@@ -144,7 +144,7 @@ while True:
         ecoh = round(bcoh+sum(leftECOH)-sum(rightECOH), 2)
         # Over or Short = Total Cash on Hand - Ending Cash on Hand
         oos = round(r[7]-ecoh, 2)
-        for item, value in zip(output_items, (bcoh, trs, lt, ttc, ecoh,oos)):
+        for item, value in zip(output_items, (bcoh, trs, lt, ttc, r[7], oos)):
             window[item].update(value)
 
 window.close()
